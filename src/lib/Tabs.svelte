50f13057a4866fe66758897de399aab1bbd0062e
Austin Tallent <austintallent25@outlook.com>
2024-08-30 16:04:40 -0700

    Add tabs. Don't they look great

--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -1,21 +1,89 @@
 <script>
- import CarouselSection from "../lib/CarouselSection.svelte";
+    export let items = [];
+    export let activeTabValue = 1;
 
- let tabs = [
-     {name: "Hello", comp: CarouselSection},
-     {name: "Goodbye", comp: false},
- ];
-
- let cur = tabs[0];
-
+    const handleClick = (tabValue) => () => (activeTabValue = tabValue);
 </script>
 
-{#each tabs as tab}
-    <button class:selected={cur === tab} on:click={() => (cur = tab)}>
-        {tab.name}
-    </button>
-{/each}
+<div class="bg">
+    <ul>
+        {#each items as item}
+            <li class={activeTabValue === item.value ? "active" : ""}>
+                <span on:click={handleClick(item.value)}>{item.label}</span>
+            </li>
+        {/each}
+    </ul>
+    {#each items as item}
+        {#if activeTabValue == item.value}
+            <div class="box">
+                <svelte:component this={item.component} />
+            </div>
+        {/if}
+    {/each}
+</div>
 
-<main>
-    <svelte:component this={cur.comp} />
-</main>
+<style>
+    .bg {
+        background-color: var(--bg-color);
+    }
+    .box {
+        border: 2px solid var(--yellow);
+        border-radius: 7px;
+        width: 90vw;
+        background-color: var(--secondary-bg-color);
+        color: var(--text-color);
+        margin-left: 5vw;
+    }
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        padding-left: 0;
+        margin-bottom: 20px;
+        margin-left: calc(5vw - 10px);
+        list-style: none;
+        padding-top: 20px;
+    }
+    li {
+        margin-bottom: -1px;
+    }
+
+    span {
+        border: 0;
+        border-radius: 7px;
+        cursor: pointer;
+        display: inline-block;
+        height: 3.25rem;
+        line-height: 3.25rem;
+        padding: 0 20px;
+        text-align: center;
+        text-decoration: none;
+        white-space: nowrap;
+        font-size: 20px;
+        margin-left: 10px;
+        margin-right: 10px;
+        background-color: var(--secondary-bg-color);
+        border: 2px solid var(--green);
+        color: var(--text-color);
+    }
+
+    span:hover {
+        border-color: var(--green-hover);
+    }
+
+    li.active > span {
+        border: 0;
+        border-radius: 7px;
+        cursor: default;
+        display: inline-block;
+        height: 3.25rem;
+        line-height: 3.25rem;
+        padding: 0 20px;
+        text-align: center;
+        text-decoration: none;
+        white-space: nowrap;
+        font-size: 20px;
+        background: linear-gradient(0deg, hsl(42 100% 50%), var(--yellow));
+        box-shadow: 0 0 15px var(--yellow);
+        color: var(--bg-color);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,33 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import CarouselSection from "$lib/CarouselSection.svelte";
+    import Welcome from "../lib/Welcome.svelte";
+    import About from "../lib/AboutHome.svelte";
+    import Tab1 from "../lib/Tab1.svelte";
+    import Tab2 from "../lib/Tab2.svelte";
+    import Tab3 from "../lib/Tab3.svelte";
+    import Tabs from "../lib/Tabs.svelte";
+
+    let count = 0;
+
+    let items = [
+        { label: "Content", value: 1, component: Tab1 },
+        { label: "Interactions", value: 2, component: Tab2 },
+        { label: "Tab 3", value: 3, component: Tab3 },
+    ];
+</script>
+
 <svelte:head>
     <title>RHS Robotics Homepage</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
-<script>
- import Header from "$lib/Header.svelte";
- import Footer from "$lib/Footer.svelte";
- import CarouselSection from "$lib/CarouselSection.svelte";
- import Welcome from "../lib/Welcome.svelte";
- import About from "../lib/AboutHome.svelte";
- import Tabs from "../lib/Tabs.svelte";
-
- let count = 0;
- </script>
-
-
-
- <!-- <Banner /> -->
+<!-- <Banner /> -->
 
 <div id="main">
     <Welcome />
     <About />
 
-
-    <Tabs />
-
+    <Tabs {items} />
 </div>
