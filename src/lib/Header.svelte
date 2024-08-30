010100f63473b3f66ba911da846c859fdc85d266
Calvin Tallent <calvintallentspam@gmail.com>
2024-08-29 23:18:57 -0700

    Style hamburger menu.

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -8,6 +8,7 @@
         position: relative;
         margin-left: 5vw;
         margin-right: 5vw;
+        justify-content: space-between;
         overflow: hidden;
     }
     .logo{
@@ -27,34 +28,35 @@
     .text:hover{
         color: var(--text-color-hover);
     }
-    .div-r{
+    .div-links{
         display: flex;
         width: 60vw;
         justify-content: flex-end;
-    }
-    .div-l{
-        display: flex;
-        width: 40vw;
-        text-align: left;
     }
     .burger-menu{
         cursor: pointer;
         font-size: 24px;
         color: var(--text-color);
         display: none;
+        padding-left: 0;
+        /* margin-left: 5vw; */
+    }
+    .burger-menu:hover{
+        color: var(--text-color-hover);
     }
     .burger-menu #sidebar-menu{
         visibility: hidden;
         text-rendering: geometricPrecision;
         position: fixed;
         top: 0;
-        right: -250px;
-        width: 200px;
+        left: -40%;
+        width: /* 10em */ 40%;
+        /* background: red; */
         height: 100%;
         transition: 0.3s;
     }
     @media only screen and (max-width: 768px) {
-        .text{
+        .div-links{
             display: none;
         }
         .burger-menu{
@@ -67,7 +69,7 @@
 
     #burger-input:checked + .burger-menu #sidebar-menu{
         visibility: visible;
-        right: 0;
+        left: 0;
     }
     #burger-input:checked ~ .overlay{
         visibility: visible;
@@ -93,26 +95,23 @@
     #sidebar-menu ul li{
         color: var(--text-color);
         list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    #sidebar-menu ul{
+        padding: 0;
+        margin-left: 5vw;
+        margin-top: 2em;
     }
 </style>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 <header>
-    <div class="div-l">
-        <img src="logo_dark.png" alt="logo" class="logo">
-    </div>
-    <div class="div-r">
-        <a href="/" class="text">About us</a>
-        <a href="/" class="text">Members</a>
-        <a href="/" class="text">Robots</a>
-        <a href="/" class="text">Join us</a>
-    </div>
     <input type="checkbox" id="burger-input" class="burger-shower">
     <label class="burger-menu" for="burger-input">
     &#9776;
         <nav id="sidebar-menu">
-            <!-- <h2>Menu</h2> -->
-
             <ul>
                 <li>Home</li>
                 <li>Members</li>
@@ -121,6 +120,17 @@
             </ul>
         </nav>
     </label>
+
+
+    <div class="div-logo">
+        <img src="logo_dark.png" alt="logo" class="logo">
+    </div>
+    <div class="div-links">
+        <a href="/" class="text">About us</a>
+        <a href="/" class="text">Members</a>
+        <a href="/" class="text">Robots</a>
+        <a href="/" class="text">Join us</a>
+    </div>
 
     <div class="overlay"></div>
 </header>
