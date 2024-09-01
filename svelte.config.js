0f2c17f5eac64e17832562f76e81b10db69a154f
Calvin Tallent <calvintallentspam@gmail.com>
2024-09-01 13:24:37 -0700

    Set up svelte for github pages.

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,13 @@ const config = {
     adapter: adapter({
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "404.html",
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
     // vite: {
     // 	optimizeDeps: {
     // 		include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
