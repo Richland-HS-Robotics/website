789f27418da26cab878be06992db4b57e965efc2
Austin Tallent <austintallent25@outlook.com>
2024-09-01 18:33:19 -0700

    Work on contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,18 +1,58 @@
+<h1>Get in touch</h1>
+<div class="container">
+  <div class="card">
+    <p>contact@rsd-robotics.com</p>
+  </div>
+  <div class="card"></div>
+  <div class="card"></div>
+  <div class="card"></div>
+</div>
 
+<p>
+  You can email us at <a href="mailto:contact@rhs-robotics.com"
+    >contact@rhs-robotics.com</a
+  >.
+</p>
 
-<h1>Contacting Richland Robotics</h1>
+<p>
+  You can look at our code on Github at <a
+    href="https://github.com/Richland-HS-Robotics"
+    >https://github.com/Richland-HS-Robotics</a
+  >.
+</p>
 
-<p> You can email us at <a
-  href="mailto:contact@rhs-robotics.com">contact@rhs-robotics.com</a>. </p>
+<p>
+  You can view our posts on Instagram at <a
+    href="https://www.instagram.com/rhs_robotics24/"
+    >https://www.instagram.com/rhs_robotics24/</a
+  >
+</p>
 
-<p> You can look at our code on Github at <a
-  href="https://github.com/Richland-HS-Robotics">https://github.com/Richland-HS-Robotics</a>.
-  </p>
+<p>
+  Also check us out at FTCScout: <a href="https://ftcscout.org/teams/15010"
+    >https://ftcscout.org/teams/15010</a
+  >
+</p>
 
-<p> You can view our posts on Instagram at <a
-  href="https://www.instagram.com/rhs_robotics24/">https://www.instagram.com/rhs_robotics24/</a>
-  </p>
-
-<p> Also check us out at FTCScout: <a
-  href="https://ftcscout.org/teams/15010">https://ftcscout.org/teams/15010</a>
-  </p>
+<style>
+  h1 {
+    padding-top: 80px;
+  }
+  .container {
+    display: flex;
+    width: 90vw;
+    background-color: var(--bg-color);
+  }
+  .card {
+    width: calc(90vw / 4 - 2vw);
+    transform: translateX(-1vw);
+    margin-left: 1vw;
+    margin-right: 1vw;
+    height: 200px;
+    border-radius: 7px;
+    border: 1px solid var(--yellow);
+    background-color: var(--secondary-bg-color);
+    display: flex;
+    box-shadow: 0 0 15px var(--secondary-bg-color);
+  }
+</style>
