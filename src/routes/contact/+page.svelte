538e46ccf709cac113f72fe9e74775f08cf20f58
Austin Tallent <austintallent25@outlook.com>
2024-09-02 08:42:27 -0700

    Add svg background

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,9 @@
+<svg id="grey" width="50vw" height="50vw">
+  <circle cx="25vw" cy="25vw" r="25vw" fill="#4e5565" />
+</svg>
+<svg id="yellow" width="50vw" height="50vw">
+  <circle cx="25vw" cy="25vw" r="25vw" fill="var(--green)" />
+</svg>
 <h1>Get in touch</h1>
 <div id="container">
   <div class="card">
@@ -6,21 +12,19 @@
       <span class="heading">Email us</span>
     </div>
 
-      <span class="contact">
-        <a href="mailto:contact@rhs-robotics.com">contact@rhs-robotics.com</a>
-      </span>
+    <span class="contact">
+      <a href="mailto:contact@rhs-robotics.com">contact@rhs-robotics.com</a>
+    </span>
   </div>
   <div class="card">
-
     <div class="heading-div">
-      <span class="fa-brands fa-instagram icon"  id="icons"></span>
+      <span class="fa-brands fa-instagram icon" id="icons"></span>
       <span class="heading">Instagram</span>
     </div>
 
     <span class="contact">
       <a href="https://www.instagram.com/rhs_robotics24/">@rhs_robotics24</a>
     </span>
-
   </div>
   <div class="card">
     <div class="heading-div">
@@ -31,7 +35,6 @@
     <span class="contact">
       <a href="https://github.com/Richland-HS-Robotics">Richland-HS-Robotics</a>
     </span>
-
   </div>
   <div class="card">
     <div class="heading-div">
@@ -47,37 +50,53 @@
   </div>
 </div>
 
-
-
-
 <p>
   Also check us out at FTCScout: <a href="https://ftcscout.org/teams/15010"
     >https://ftcscout.org/teams/15010</a
   >
 </p>
 
-
 <style>
-  p{
+  #grey {
+    position: absolute;
+    top: -25px;
+    left: -15vw;
+    z-index: 0;
+    filter: blur(150px);
+    opacity: 0.5;
+  }
+  #yellow {
+    position: absolute;
+    top: 55px;
+    left: 65vw;
+    z-index: 0;
+    filter: blur(250px);
+    opacity: 0.25;
+  }
+  p {
     padding-top: 50px;
     padding-bottom: 200px;
+    position: relative;
+    z-index: 2;
   }
-  #icons{
+  #icons {
     margin-left: 2.5vw;
   }
-  .icon{
+  .icon {
     color: var(--text-color);
     font-size: 40px;
     /* margin-top: 5%; */
   }
- .heading-div{
-   margin-top: 2.5vw;
-   display: flex;
-   flex-direction: column;
- }
+  .heading-div {
+    margin-top: 2.5vw;
+    display: flex;
+    flex-direction: column;
+  }
   h1 {
     padding-top: 80px;
     padding-bottom: 50px;
+    z-index: 2;
+    position: relative;
   }
   #container {
     display: flex;
@@ -96,7 +115,7 @@
     background-color: var(--secondary-bg-color);
     box-shadow: 0 0 15px var(--secondary-bg-color);
   }
-  .heading{
+  .heading {
     font-size: 30px;
     /* margin-left: auto;
        margin-right: 25%; */
@@ -105,7 +124,7 @@
     margin-left: 2.5vw;
     margin-top: 30px;
   }
-  .contact{
+  .contact {
     color: var(--text-color);
     font-size: 16px;
     width: 100%;
@@ -116,26 +135,26 @@
     max-width: calc(90vw / 4 - 7vw);
   }
   @media screen and (max-width: 736px) {
-    #container{
+    #container {
       flex-direction: column;
     }
-    .card{
+    .card {
       width: 86vw;
       margin-bottom: 4vw;
       margin-top: 4vw;
     }
-    .contact{
+    .contact {
       margin-left: 5vw;
       max-width: 81vw;
       margin-bottom: 5vw;
     }
-    #icons{
+    #icons {
       margin-left: 5vw;
     }
-    .heading{
+    .heading {
       margin-left: 5vw;
     }
-    p{
+    p {
       padding-bottom: 25px;
     }
   }
